c8c9703f0207dd46e996283a6b582054ec630675
Alex Gim <areygim@gmail.com>
2020-09-02 15:54:49 +0900

    add: date in thumbnail

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -8,6 +8,7 @@ export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
+      <p id="thumbnail-date">{node.frontmatter.date || node.fields.slug}</p>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
