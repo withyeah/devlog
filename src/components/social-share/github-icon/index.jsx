615c775c8211a951ac2dadfbf0625118e3799f9c
Alex Gim <areygim@gmail.com>
2020-09-04 16:17:13 +0900

    fix: github address on top social-share button

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/withyeah/devlog"
+      href="https://github.com/withyeah"
       className="github"
       aria-label="GitHub"
     >
